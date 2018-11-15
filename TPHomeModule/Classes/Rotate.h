76840deb87fdd0ea67b640107ffc52a276fc2662
smile <smile@smiledeiMac.local>
2018-11-15 16:58:37 +0800

    fff

---
+++ TPHomeModule/Classes/Rotate.h
@@ -0,0 +1,16 @@
+//
+//  Rotate.h
+//  Pods-TPHomeModule_Example
+//
+//  Created by smile on 2018/11/15.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Rotate : UIImageView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TPHomeModule/Classes/Rotate.m
@@ -0,0 +1,20 @@
+//
+//  Rotate.m
+//  Pods-TPHomeModule_Example
+//
+//  Created by smile on 2018/11/15.
+//
+
+#import "Rotate.h"
+
+@implementation Rotate
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
